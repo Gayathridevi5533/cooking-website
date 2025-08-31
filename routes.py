37ede6e31a113b0ceb6b27d6888d5ba73434f713
Gayathridevi5533 <gayathrideviganesh5533@gmail.com>
2025-09-01 09:34:38 +1200

    creating files and copying handwritten codes based on reference (pizza website) and previous website

--- routes.py
+++ routes.py
@@ -2,10 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#Home page 
 @app.route('/')
-def home():
-    return render_template('Home.html')
+def root():
+    return render_template('home.html', page_title='HOME')
+
+@app.route('/south indian')
+def southindian():
+    return render_template('south indian.html', page_title='SOUTHINDIAN')
+
+@app.route('/north indian')
+def northindian():
+    return render_template('north indian.html', page_title='NORTHINDIAN')
+
+@app.route('/street food')
+def streetfood():
+    return render_template('street food.html', page_title='STREET FOOD')
+
+@app.route('/one pot')
+def onepot():
+    return render_template('one pot.html', page_title='ONE POT')
+
+@app.route('/regional')
+def regional():
+    return render_template('regional.html', page_title='REGIONAL')
+
+@app.route('/desserts')
+def desserts():
+    return render_template('desserts.html', page_title='DESSERTS')
+
 
 if (__name__) == ("__main__"):
     app.run(debug=True)
