3517feb88b2d2c36efda8d8d3cf5e636f342b12b
Gayathridevi5533 <gayathrideviganesh5533@gmail.com>
2025-09-01 19:57:32 +1200

    writting comments

--- routes.py
+++ routes.py
@@ -3,30 +3,37 @@ import sqlite3
 
 app = Flask(__name__)
 
+#root route
 @app.route('/')
 def root():
     return render_template('home.html', page_title='HOME')
 
+#south indian route - called by SOUTH INDIAN in the the nav bar and returns information about the site 
 @app.route('/south indian')
 def southindian():
-    return render_template('south indian.html', page_title='SOUTHINDIAN')
+    return render_template('south indian.html', page_title='SOUTH INDIAN')
 
+#norht indian route - called by NORTH INDIAN in the the nav bar and returns information about the site 
 @app.route('/north indian')
 def northindian():
-    return render_template('north indian.html', page_title='NORTHINDIAN')
+    return render_template('north indian.html', page_title='NORTH INDIAN')
 
+#street food route - called by STREET FOOD in the the nav bar and returns information about the site 
 @app.route('/street food')
 def streetfood():
     return render_template('street food.html', page_title='STREET FOOD')
 
+#one pot route - called by ONE POT in the the nav bar and returns information about the site 
 @app.route('/one pot')
 def onepot():
     return render_template('one pot.html', page_title='ONE POT')
 
+#regional route - called by REGIONAL in the the nav bar and returns information about the site 
 @app.route('/regional')
 def regional():
     return render_template('regional.html', page_title='REGIONAL')
 
+#desserts route - called by DESSERTS in the the nav bar and returns information about the site 
 @app.route('/desserts')
 def desserts():
     return render_template('desserts.html', page_title='DESSERTS')
@@ -44,6 +51,7 @@ def all_recipes():
     conn.close()
     return render_template('all_recipes.html',page_title='ALL Recipes',recipes=recipes)
 
+#maps the recipe to show its description and other details
 @app.route('/recipe/<int:id>')
 def recipe (id):
     conn = sqlite3.connect('cooking.db')
