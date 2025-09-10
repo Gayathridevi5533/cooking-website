35212ffd1ca1ea3d84d41edb72c70974c525ba7c
Gayathridevi5533 <gayathrideviganesh5533@gmail.com>
2025-09-10 17:05:33 +1200

    fixing problems

--- routes.py
+++ routes.py
@@ -4,7 +4,8 @@ import sqlite3
 
 app = Flask(__name__)
 
-#root route
+
+# root route
 @app.route('/')
 def root():
     conn = sqlite3.connect('cooking.db')
@@ -12,17 +13,24 @@ def root():
     cur.execute('SELECT id, name FROM Category ORDER BY name ASC;')
     # fetchall returns a list of results 
     cuisines = cur.fetchall()
-    print(cuisines) #DEBUG
+    print(cuisines)  # DEBUG
     conn.close()
-    return render_template('cooking-website.html', page_title='HOME', cuisines=cuisines)
+    return render_template('cooking-website.html',
+                           page_title='HOME',
+                           cuisines=cuisines)
 
-#south indian route - called by SOUTH INDIAN in the the nav bar and returns information about the site 
+
+# south indian route - called by SOUTH INDIAN in the
+#  nav bar and returns information about the site
 @app.route('/api/recipes', methods=['POST'])
 def recipes():
     conn = sqlite3.connect('cooking.db')
     cur = conn.cursor()
     cur.execute(
-        "SELECT r.id, r.name, r.description, r.difficulty, r.servings, r.cookTime, r.category, c.name as categoryName, image FROM Recipe r INNER JOIN Category AS c ON r.category = c.id ORDER BY r.name ASC;"
+        "SELECT r.id, r.name, r.description, r.difficulty, r.servings,"
+        " r.cookTime, r.category, c.name as categoryName,"
+        " image FROM Recipe r INNER JOIN Category AS c ON r.category = c.id "
+        "ORDER BY r.name ASC;"
     )    
     # fetchall returns a list of results 
     rows = cur.fetchall()
@@ -39,30 +47,42 @@ def recipes():
         "category": row[7]
     } for row in rows]
 
-    #recipes = [{"id": row[0], "title":row[1] ,"description":row[2], "difficulty":row[3], "serving":[4], "cookTime":[5],"image":"https://ministryofcurry.com/wp-content/uploads/2023/05/sambar-11.jpg","category":"South Indian"} for row in rows]  # Example data
+    # recipes = [{"id": row[0], "title":row[1] ,"description":row[2], 
+    # "difficulty":row[3], "serving":[4], "cookTime":[5],
+    # "image":
+    # https://ministryofcurry.com/wp-content/uploads/2023/05/sambar-11.jpg,
+    # "category":"South Indian"} for row in rows]  # Example data
     return jsonify(recipes)
-#list all the cuisines and recipes (in alphabetic order)
 
-#eventually link each one to a details page 
+
+# list all the cuisines and recipes (in alphabetic order)
+# eventually link each one to a details page
 @app.route('/all_recipes')
 def all_recipes():
     conn = sqlite3.connect('cooking.db')
     cur = conn.cursor()
     cur.execute('SELECT id , name FROM Recipe ORDER BY name ASC;')
     # fetchall- returns a list of results 
-    recipes= cur.fetchall()
-    print(recipes) #DEBUG
+    recipes = cur.fetchall()
+    print(recipes)  # DEBUG
     conn.close()
-    return render_template('all_recipes.html',page_title='ALL Recipes',recipes=recipes)
+    return render_template('all_recipes.html', 
+                           page_title='ALL Recipes',
+                           recipes=recipes)
+
 
 @app.route('/api/recipe', methods=['POST'])
 def recipe():
     conn = sqlite3.connect('cooking.db')
     cur = conn.cursor()
     cur.execute(
-        "SELECT r.id, r.name, r.description, r.difficulty, r.servings, r.cookTime, r.category, ii.ingredient, ii.instruction, r.image, c.name as categoryName FROM Category c INNER JOIN Recipe r ON c.id = r.category INNER JOIN IngredientsAndInstructions AS ii ON r.id = ii.recipe_id ORDER BY r.name ASC;"
-    )    
-    # fetchall returns a list of results 
+        "SELECT r.id, r.name, r.description, r.difficulty, r.servings,"
+        " r.cookTime, r.category, ii.ingredient, ii.instruction, r.image,"
+        " c.name as categoryName FROM Category c INNER JOIN Recipe r ON "
+        "c.id = r.category INNER JOIN IngredientsAndInstructions AS ii ON "
+        "r.id = ii.recipe_id ORDER BY r.name ASC;"
+    )
+    # fetchall returns a list of results
     rows = cur.fetchall()
     conn.close()
 
@@ -71,8 +91,11 @@ def recipe():
         ingredients_str = row[7]
         instructions_str = row[8]
         
-        ingredients_list = [item.strip() for item in ingredients_str.split(',')] if ingredients_str else []
-        instructions_list = [item.strip() for item in instructions_str.split(',')] if instructions_str else []
+        ingredients_list = [item.strip() for item in ingredients_str.split(',')
+                            ] if ingredients_str else []
+        instructions_list = [item.strip() for item in 
+                             instructions_str.split(',')
+                            ]if instructions_str else []
 
         recipes.append({
             "id": row[0],
