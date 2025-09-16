74dc58a607126078e15230a6f8ba812eaa81450a
Gayathridevi5533 <gayathrideviganesh5533@gmail.com>
2025-09-17 10:54:21 +1200

    checking and correcting errors

--- static/js/cooking-script.js
+++ static/js/cooking-script.js
@@ -1,69 +1,6 @@
 // Recipe data
 var recipes = [];
 
-const recipes1 = [
-    {
-        id: 1,
-        title: "Idly Sambar",
-        description: "Idly Sambar is a beloved staple of South Indian cuisine—a comforting, nutritious combo of soft, steamed idlis served with a flavorful, tangy lentil stew called sambar.",
-        cookTime: "30 - 35 min",
-        servings: 4,
-        difficulty: "Easy",
-        image: "https://ministryofcurry.com/wp-content/uploads/2023/05/sambar-11.jpg",
-        category: "South Indian"
-    },
-    {
-        id: 2,
-        title: "Pani puri",
-        description: "Pani puri is a popular Indian street food consisting of crispy, hollow puri filled with a flavorful mixture of potatoes, chickpeas, and chutneys, then immersed in spicy and tangy pani (flavored water). ",
-        cookTime: "60 - 90 min",
-        servings: 4,
-        difficulty: "Hard",
-        image: "https://www.jcookingodyssey.com/wp-content/uploads/2025/06/Pani-Puri-Recipe.jpg",
-        category: "Street food"
-    },
-    {
-        id: 3,
-        title: "Biriyani",
-        description: " Biryani is a rich tapestry of regional flavors, each with its own unique twist on the classic dish. While the core elements—fragrant rice, marinated meat, and aromatic spices—remain consistent, the variations reflect the diverse culinary traditions of the region.",
-        cookTime: "45 - 60 min",
-        servings: 4,
-        difficulty: "Medium",
-        image: "https://www.shutterstock.com/image-photo/traditional-chicken-biryani-served-brass-600nw-2622739739.jpg",
-        category: "One pot"
-    },
-    {
-        id: 4,
-        title: "Chicken Tandoori",
-        description: "Tandoori chicken is a renowned Indian dish celebrated for its vibrant flavor and tender texture.It is traditionally cooked in a tandoor, a cylindrical clay oven that imparts a unique smoky aroma and charred finish to the meat.",
-        cookTime: "25 - 45min",
-        servings: 4,
-        difficulty: "Easy",
-        image: "https://www.allrecipes.com/thmb/ygY1JXP8_IkDSjPPW5VH2dTiMMU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/50347-indian-tandoori-chicken-DDMFS-4x3-3035-205e98c80b2f4275b5bd010c396d9149.jpg",
-        category: "North Indian"
-    },
-    {
-        id: 5,
-        title: "Butter Chicken",
-        description: "A creamy, tomato-based gravy with tender chicken pieces, often considered a gateway to Indian cuisine. ",
-        cookTime: "35 - 45 min",
-        servings: 4,
-        difficulty: "Medium",
-        image: "https://feelgoodfoodie.net/wp-content/uploads/2023/04/Easy-Butter-Chicken-11.jpg",
-        category: "Regional"
-    },
-    {
-        id: 6,
-        title: "Rasgulla",
-        description: "Rasgulla is an iconic syrupy dessert from eastern India—made of soft, spongy ball‑shaped dumplings crafted from fresh cottage cheese (chhena) and soaked in fragrant light sugar syrup.",
-        cookTime: "45 - 60 mins",
-        servings: 4,
-        difficulty: "Medium",
-        image: "https://images.immediate.co.uk/production/volatile/sites/30/2024/09/RasGulla-2432193.jpg?quality=90&resize=708,643",
-        category: "Dessert"
-    }
-];
-
 // State
 let selectedCategory = "All";
 
